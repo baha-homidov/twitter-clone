31a639cabe4e0e7e7aebe8e516dc63ebe8e4c0f3
Mirzobakhtiyor Homidov <khamidov.mirzo@gmail.com>
2022-10-01 15:19:49 +0800

    initial commit for <Messages />

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function Messages() {
   return (
     <div className="messages-container">
-      <h1>Messages</h1>
+      <div className="top-bar">
+        <h1 className="title">Messages</h1>
+        <Link className="link">
+          <div className="profile-icon"></div>
+        </Link>
+      </div>
+      <div className="welcome-container">
+        <h2 className="welcome">Welcome to your inbox!</h2>
+        <div>
+          Drop a line with conversations between you and others on Barker!
+        </div>
+        <button className="write-message">Write a message</button>
+      </div>
     </div>
-  )
-
-
+  );
 }
