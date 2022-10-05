c675343fca3db584fcfbe3c87fda395da55b8aeb
Mirzobakhtiyor Homidov <khamidov.mirzo@gmail.com>
2022-10-05 22:11:21 +0800

    fix back button in <UserProfile />

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,6 +1,7 @@
 import userPhoto from "../assets/img/icons/placeholder-userphoto.png";
 import "../assets/css/UserProfile.css";
 import Tweet from "./Tweet";
+import { Link } from "react-router-dom";
 export default function UserProfile() {
   const arr = []; // placeholder for rendering some tweets
   for (let i = 0; i < 12; i++) {
@@ -13,20 +14,22 @@ export default function UserProfile() {
   return (
     <div className="user-profile-container">
       <div className="top-bar">
-        <button className="back">
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentColor"
-              d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"
-            ></path>
-          </svg>
-        </button>
+        <Link to="/home" className="link">
+          <button className="back">
+            <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"
+              ></path>
+            </svg>
+          </button>
+        </Link>
         <div className="info-wrapper">
           <div className="name">Alex Smith</div>
           <div className="tweets-num">12 tweets</div>
